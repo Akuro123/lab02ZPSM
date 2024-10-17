ea1530881857f017e03529cfa40bf0e025b58bdf
Jakub Wiatr <jakub.wiatr@zsdt.edu.pl>
2024-10-17 21:35:07 +0200

    MIN MAX

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const _ = require('lodash');
 
 const numbers = [10, 20, 30, 40, 50];
 const meanValue = _.mean(numbers);
+const minValue=_.min(numbers);
+const maxValue=_.max(numbers);
 
 console.log("Srednia: "+meanValue);
+console.log("Min: "+minValue);
+console.log("MAX: "+maxValue);
+
